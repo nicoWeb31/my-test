274d06e712732b98bf0149e4793ac585e5c9316e
Nicolas <Nicolas.RIOT@fittingbox.com>
2022-12-05 19:21:22 +0100

    feat(): service math test

--- libs/shared/math/utils/src/lib/service/math.service.spec.ts
+++ libs/shared/math/utils/src/lib/service/math.service.spec.ts
@@ -6,11 +6,21 @@ describe('MathService', () => {
   let service: MathService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [MathService, { provide: 'numFromCaller', useValue: 15 }],
+    });
     service = TestBed.inject(MathService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be equal to 25', () => {
+    expect(service.addFromCaller(10)).toBe(25);
+  });
+
+  it('should be equal to 15', () => {
+    expect(service.addFromCaller(0)).toBe(15);
   });
 });
--- libs/testv15/catfact/feature/src/lib/testv15-catfact-feature/cat-fact.routes.ts
+++ libs/testv15/catfact/feature/src/lib/testv15-catfact-feature/cat-fact.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { MathService } from '@my-test/shared/math/utils';
+
 import { Testv15CatfactFeatureComponent } from './testv15-catfact-feature.component';
 
 export const JSON_PL_ROUTES: Routes = [
