410cce712f5e8fc5e18071f46929163f6f847f7b
Nicolas <Nicolas.RIOT@fittingbox.com>
2022-12-07 19:31:11 +0100

    chore(): test service http

--- libs/testv15/catfact/data-access/src/lib/service/cat-fact.service.spec.ts
+++ libs/testv15/catfact/data-access/src/lib/service/cat-fact.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { CatFactService } from './cat-fact.service';
+import { mockBreed } from './mock/breeds.mock';
 
 describe('CatFactService', () => {
   let service: CatFactService;
+  let httpTestingController: HttpTestingController;
+  const mockUrl = 'https://catfact.ninja/breeds';
+  const mockBreedResponse = mockBreed;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [CatFactService],
+      imports: [HttpClientTestingModule],
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(CatFactService);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should  fetch cat breed data', (done) => {
+    service.catBreeds$.subscribe((res) => {
+      expect(res).toEqual(mockBreedResponse.data);
+      done();
+    });
+
+    const req = httpTestingController.expectOne(mockUrl);
+    expect(req.request.method).toEqual('GET');
+
+    // Provide each request with a mock response
+    req.flush(mockBreedResponse);
   });
 });
--- libs/testv15/json-placeholder/data-access/src/lib/service/json-placeholder.service.spec.ts
+++ libs/testv15/json-placeholder/data-access/src/lib/service/json-placeholder.service.spec.ts
@@ -1,13 +1,42 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { JsonPlaceholderService } from './json-placeholder.service';
+import { mockPost } from './mock/posts.mock';
 
 describe('JsonPlaceholderService', () => {
   let service: JsonPlaceholderService;
+  let httpTestingController: HttpTestingController;
+  const mockPostResponse = mockPost;
+  const mockUrl = 'https://jsonplaceholder.typicode.com/posts';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [JsonPlaceholderService],
+      imports: [HttpClientTestingModule],
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(JsonPlaceholderService);
+  });
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should json placeholder post', (done) => {
+    service.post$.subscribe((res) => {
+      expect(res).toEqual(mockPostResponse);
+      done();
+    });
+
+    const req = httpTestingController.expectOne(mockUrl);
+    expect(req.request.method).toEqual('GET');
+
+    // Provide each request with a mock response
+    req.flush(mockPostResponse);
   });
 
   it('should be created', () => {
