64c1f76877030c3bbcd0077cf5106ea62400cbdd
Nicolas <Nicolas.RIOT@fittingbox.com>
2022-12-09 18:46:02 +0100

    chore(): try simple dom test

--- libs/testv15/json-placeholder/feature/src/lib/testv15-json-placeholder-feature/testv15-json-placeholder-feature.component.spec.ts
+++ libs/testv15/json-placeholder/feature/src/lib/testv15-json-placeholder-feature/testv15-json-placeholder-feature.component.spec.ts
@@ -1,14 +1,18 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore } from '@ngrx/store/testing';
 import { Testv15JsonPlaceholderFeatureComponent } from './testv15-json-placeholder-feature.component';
-
 describe('Testv15JsonPlaceholderFeatureComponent', () => {
   let component: Testv15JsonPlaceholderFeatureComponent;
   let fixture: ComponentFixture<Testv15JsonPlaceholderFeatureComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Testv15JsonPlaceholderFeatureComponent],
+      imports: [
+        Testv15JsonPlaceholderFeatureComponent,
+        HttpClientTestingModule,
+      ],
+      providers: [provideMockStore({})],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Testv15JsonPlaceholderFeatureComponent);
@@ -18,5 +22,17 @@ describe('Testv15JsonPlaceholderFeatureComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain <p>', () => {
+    const element = fixture.nativeElement;
+    const p = element.querySelector('p');
+    expect(p).toBeTruthy();
+  });
+
+  it('should not contain <div>', () => {
+    const element = fixture.nativeElement;
+    const p = element.querySelector('div');
+    expect(p).toBeFalsy();
   });
 });
