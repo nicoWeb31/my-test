cd366fffa1c2f87e32aa737c4a7be4984b7093c1
Nicolas <Nicolas.RIOT@fittingbox.com>
2022-12-19 20:55:44 +0100

    feat(): composition directive, underline

--- libs/testv15/directive-composition/feature/src/lib/directives/uderline.directive.ts
+++ libs/testv15/directive-composition/feature/src/lib/directives/uderline.directive.ts
@@ -1,6 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, inject } from '@angular/core';
 
 @Directive({
   selector: '[myTestUderline]',
+  standalone: true
 })
-export class UderlineDirective {}
+export class UderlineDirective {
+  htmlElement = inject(ElementRef<HTMLElement>);
+
+  @HostListener('mouseenter')
+  mousseIn() {
+    this.htmlElement.nativeElement.style.textDecoration = 'underline'
+  }
+}
--- libs/testv15/directive-composition/feature/src/lib/testv15-directive-composition-feature/testv15-directive-composition-feature.component.ts
+++ libs/testv15/directive-composition/feature/src/lib/testv15-directive-composition-feature/testv15-directive-composition-feature.component.ts
@@ -1,15 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoldDirective } from '../directives/bold.directive';
+import { UderlineDirective } from '../directives/uderline.directive';
 
 //NOTE: https://www.youtube.com/watch?v=2S6FVt-tQ7M
 
 @Component({
   selector: 'my-test-testv15-directive-composition-feature',
   standalone: true,
-  imports: [CommonModule,BoldDirective],
+  imports: [CommonModule,BoldDirective,UderlineDirective],
   template: `
-  <div myTestBold>
+  <div myTestBold myTestUderline>
     test directive 
   </div>
   `,
