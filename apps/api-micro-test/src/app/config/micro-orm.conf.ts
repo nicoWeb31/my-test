5101c9db0279b1903b4a22856b0548826c706597
Nicolas <Nicolas.RIOT@fittingbox.com>
2023-01-12 20:16:35 +0100

    fix: docker img

--- apps/api-micro-test/src/app/config/micro-orm.conf.ts
+++ apps/api-micro-test/src/app/config/micro-orm.conf.ts
@@ -1,9 +1,10 @@
 import { Options } from '@mikro-orm/core';
+// https://abdlrahmansaber.medium.com/nestjs-setting-up-mikroorm-postgresql-c3e26b9c069e
 
 const config = {
   dbName: 'test',
   type: 'postgresql',
-  host: '127.0.0.1',
+  host: 'localhost',
   port: 5432,
   username: 'root',
   password: 'root',
