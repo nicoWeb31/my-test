23f0c1296e256a992d332b6a40f7a4db01da9c2c
Nicolas <Nicolas.RIOT@fittingbox.com>
2023-01-19 08:49:13 +0100

    fix(): micro orm

--- apps/api-micro-test/src/app/app.module.ts
+++ apps/api-micro-test/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import config from './config/micro-orm.conf';
+// import config from './config/micro-orm.conf';
 
 // https://wanago.io/2022/05/23/api-nestjs-mikroorm-postgresql/
+
 
 @Module({
   imports: [
     MikroOrmModule.forRoot({
-      ...config,
+      // ...config,
+      type: 'postgresql',
+      clientUrl: 'postgresql://root:root@localhost:5432/test',
       autoLoadEntities: true,
+      
     }),
     UsersModule,
   ],
--- apps/api-micro-test/src/app/config/micro-orm.conf.ts
+++ apps/api-micro-test/src/app/config/micro-orm.conf.ts
@@ -2,12 +2,12 @@ import { Options } from '@mikro-orm/core';
 // https://abdlrahmansaber.medium.com/nestjs-setting-up-mikroorm-postgresql-c3e26b9c069e
 
 const config = {
-  dbName: 'test',
   type: 'postgresql',
   host: 'localhost',
   port: 5432,
   username: 'root',
   password: 'root',
+  dbName: 'test',
   debug: true,
 } as Options;
 
